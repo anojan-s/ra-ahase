cfbe01186898af9208fd82aa767155cbe01c1f31
Anojan Sivaranjan <anojan@Anojans-MacBook-Pro.local>
2020-12-22 14:58:43 +0530

    gulp build setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
-  
-  exports.default = defaultTask
+'use strict';
+
+//required gulp plugins
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+  return gulp.src('./src/scss/**/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./assets/css'))
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/scss/**/*.scss', ['sass']);
+});
