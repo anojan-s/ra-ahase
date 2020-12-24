c6cfb349e4e9d60880c34cad90f4b8b74f98e9f0
Anojan Sivaranjan <anojan@Anojans-MacBook-Pro.local>
2020-12-24 11:30:12 +0530

    messing around with gulp and browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,60 @@
 'use strict';
 
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src(".src/scss/**/*.scss")
+      .pipe(sass())
+      .pipe(gulp.dest("./assets/css"))
+      .pipe(browserSync.stream());
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', gulp.series('sass' ,function(done) {
+    browserSync.init({
+        server: "."
+    });
+
+    gulp.watch("./src/scss/**/*.scss", ['sass']);
+    gulp.watch("./**/*.html");
+    browserSync.reload();
+    done();
+}));
+
+gulp.task('default', gulp.series('serve'));
+
+
+// Archived code
+/* 
 //required gulp plugins
-const gulp = require('gulp');
-const sass = require('gulp-sass');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
-gulp.task('sass', function(){
-  return gulp.src('./src/scss/**/*.scss')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('./assets/css'))
-});
+//browserSync initialization
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: '.'
+    },
+    open: false,
+    notify: false
+  })
+})
+*/
 
-gulp.task('sass:watch', function () {
-  return gulp.watch('./src/scss/**/*.scss', { ignoreInitial: false }, { queue: false }, ['sass']);
-});
+//gulp.task('sass', function(){ 
+  //return gulp.src('./src/scss/**/*.scss')
+  //.pipe(sass().on('error', sass.logError))
+  //.pipe(gulp.dest('./assets/css'))
+  //.pipe(browserSync.stream());
+//});
+
+//gulp.task('watch', gulp.series('sass','browserSync', function () {
+  //gulp.watch('./src/scss/**/*.scss', ['sass']);
+  //gulp.watch('./**/*.html', browserSync.reload); 
+  //gulp.watch('./js/**/*.js', browserSync.reload); 
+//})); 
